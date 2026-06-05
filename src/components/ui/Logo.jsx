704c485a618b4939b7e9c38cf861763b7f82be00
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-06-05 14:18:42 +0600

    Add code icon for logo

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,10 +1,12 @@
+import { LuCodeXml } from "react-icons/lu";
 
 
 const Logo = () => {
     return (
-        <>
+        <div className="flex gap-2 items-center">
+            <LuCodeXml className="text-2xl" />
             <h2 className="font-semibold text-lg uppercase">Md Shakil Ahmed</h2>
-        </>
+        </div>
     );
 };
 
